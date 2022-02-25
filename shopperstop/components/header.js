4a91f80bf970b8ab0cd875f684d9dc2e62df7b44
Kartiksaini88 <97525903+Kartiksaini88@users.noreply.github.com>
2022-02-25 19:33:18 +0530

    Add files via upload

---
+++ shopperstop/components/header.js
@@ -0,0 +1,36 @@
+function header(){
+    return ` 
+    <div id="top_header">
+      <div id="store_contact"><a id="store_a"> <span><img id="home_logo" src="https://cdn-icons-png.flaticon.com/512/3443/3443338.png"></span> All Store</a>  
+        <a id="contact_a"> <span><img id="home_logo" src="https://cdn-icons-png.flaticon.com/512/1250/1250646.png"></span> Contact Us</a></div>
+        <div><img id="name_logo" src="https://www.shoppersstop.com/_ui/responsive/common/assets/images/logo.svg"></div>
+         <div id="right_head">
+        <div id="input_bar"> 
+        
+        <input id="search_bar" type="text" placeholder="Search Products & Brands">
+          <img id="search_btn" src="https://cdn-icons-png.flaticon.com/128/54/54481.png"></div>
+        
+        <div> <a href="wishlist.html"><img id="page_logo"  src="https://cdn-icons-png.flaticon.com/128/535/535285.png" ></a> </div>
+        <div> <a href="cart.html"><img id="page_logo"  src="https://cdn-icons-png.flaticon.com/128/102/102665.png"></a> </div>
+        <div> <a href="signin.html"><img id="page_logo"  src="https://cdn-icons-png.flaticon.com/512/1946/1946429.png"></a> </div>
+      </div>
+    </div>
+    
+
+    <div id="sub_header">
+      <a>BARGAINS</a>
+      <a href="men.html">MENS</a>
+      <a href="women.html">WOMEN</a>
+      <a href="kids.html">KIDS</a>
+      <a href="beauty.html">BEAUTY</a>
+      <a href="homestop.html">HOMESTOP</a>
+      <a href="watch.html">WATCHES</a>
+      <a href="brands.html">BRANDS</a>
+      <a href="gift.html">GIFTS</a>
+      <a href="luxe.html">LUXE</a>
+
+    </div>
+    `
+}
+
+export default header;
